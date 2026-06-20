1dfc1ef329cbf8f5dfb0aa31119a21184bb0f153
imaad-kotadia <imaadkotadia@gmail.com>
2026-06-20 21:56:16 +0400

    Fixed invalid parsing bug by passing parser

--- __main__.py
+++ __main__.py
@@ -1,19 +1,9 @@
-import sys
 import modules.parser as parse
 
 if __name__ == "__main__":
     print("markdown-to-styled-html v1.0.0")
 
-    parser = parse.createParser()
-    parser.add_argument("mandatory", type=str, help="This argument is required")
-    parser.add_argument("optional", type=str, nargs="?", default="default_value", help="This argument is optional")
-
-    keyword_flags = {}
-
-    if len(sys.argv) == 1:
-        args, keyword_flags = parse.parserInput()
-    else: 
-        args, keyword_flags = parse.scriptInput()
+    args, keyword_flags = parse.getInput()
 
     # 4. Print results
     print(f"Mandatory: {args.mandatory}")
--- modules/parser.py
+++ modules/parser.py
@@ -1,17 +1,16 @@
 import argparse
+import sys
 
 def createParser():
-    """
-    
-    """
     return argparse.ArgumentParser(
                     prog='ProgramName',
                     description='What the program does',
                     epilog='Text at the bottom of help')
 
-def parserInput():    
+def parserInput(parser):    
     user_mandatory = input("Enter .md file: ").strip()
     user_optional = input("Enter css styling (Press Enter to skip): ").strip()
+    keyword_flags = {}
     
     # If they skipped the optional one, don't pass it to the list
     if user_optional:
@@ -20,7 +19,7 @@ def parserInput():
         simulated_args = [user_mandatory]
         
     # Overwrite the empty system arguments with our manual list
-    args, unknown = parser.parse_known_args(simulated_args)
+    args, _ = parser.parse_known_args(simulated_args)
 
     print("\n--- Add Custom Flags (Optional) ---")
     while True:
@@ -31,7 +30,7 @@ def parserInput():
         keyword_flags[key] = val if val else True  # Store True if value is skipped
     return args, keyword_flags
     
-def scriptInput():
+def scriptInput(parser):
     args, unknown_flags = parser.parse_known_args()
 
     # 3. Convert remaining arbitrary flags into a clean dictionary
@@ -40,4 +39,17 @@ def scriptInput():
         flag = unknown_flags[i].lstrip("-")  # Remove the dashes
         value = unknown_flags[i+1] if i+1 < len(unknown_flags) else True
         keyword_flags[flag] = value
+    return args, keyword_flags
+
+def getInput():
+    parser = createParser()
+    parser.add_argument("mandatory", type=str, help="This argument is required")
+    parser.add_argument("optional", type=str, nargs="?", default="default_value", help="This argument is optional")
+
+    keyword_flags = {}
+
+    if len(sys.argv) == 1:
+        args, keyword_flags = parserInput(parser)
+    else: 
+        args, keyword_flags = scriptInput(parser)
     return args, keyword_flags
